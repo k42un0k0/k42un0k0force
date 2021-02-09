bfae591a771fd8b31ea6020e872c7d3811ef83d0
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-02-09 16:55:27 +0900

    feat(front): :sparkles: add new screens

---
+++ front/lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:k42un0k0force/screens/auth/login_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("this is Home Screen"),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                },
+                child: Text('go Login Page'))
+          ],
+        ),
+      ),
+    );
+  }
+}
