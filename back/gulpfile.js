b4ce18a4336e346297164b924ae032990eb9db5c
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-02-16 17:09:08 +0900

    feat(back): build images

--- back/gulpfile.js
+++ back/gulpfile.js
@@ -1,25 +1,36 @@
 const gulp = require('gulp');
 const babel = require("gulp-babel");
-const browserSync = require('browser-sync').create();
 const environments = require('gulp-environments');
 const uglifycss = require('gulp-uglifycss');
 const terser = require('gulp-terser');
 const postcss = require('gulp-postcss');
 const purgecss = require('gulp-purgecss');
+const changed  = require('gulp-changed');
+const imagemin = require('gulp-imagemin');
+const imageminJpg = require('imagemin-jpeg-recompress');
+const imageminPng = require('imagemin-pngquant');
+const imageminGif = require('imagemin-gifsicle');
+const svgmin = require('gulp-svgmin');
 
 const production = environments.production;
 
-gulp.task('watch', () => {
-    browserSync.init({
-        proxy: 'localhost:8080',
-    });
+const distPath = 'src/main/resources/static';
 
-    gulp.watch(['assets/**/*.css'], gulp.series('copy-css-and-reload'));
-    gulp.watch(['assets/**/*.js'], gulp.series('copy-js-and-reload'));
+const generateAssetsPath = (ext)=>`assets/**/*.${ext}`
+
+const cssPath = generateAssetsPath("css")
+const jsPath = generateAssetsPath("js")
+const imgPath = generateAssetsPath("+(png|jgp|jpeg|gif)")
+
+gulp.task('watch', () => {
+    gulp.watch([cssPath], gulp.series('copy-css'));
+    gulp.watch([jsPath], gulp.series('copy-js'));
+    gulp.watch([imgPath], gulp.series('copy-img'));
+//    gulp.watch(['assets/**/*.svg'], gulp.series('copy-svg'));
 });
 
 gulp.task('copy-css', () =>
-    gulp.src(['assets/**/*.css'])
+    gulp.src([cssPath])
         .pipe(postcss([
             require('tailwindcss'),
             require('autoprefixer'),
@@ -28,23 +39,36 @@ gulp.task('copy-css', () =>
             content: ['src/main/resources/templates/**/*.html']
         })))
         .pipe(production(uglifycss()))
-        .pipe(gulp.dest('src/main/resources/static'))
+        .pipe(gulp.dest(distPath))
 );
 
 gulp.task('copy-js', () =>
-    gulp.src(['assets/**/*.js'])
+    gulp.src([jsPath])
         .pipe(babel())
         .pipe(production(terser()))
-        .pipe(gulp.dest('src/main/resources/static'))
+        .pipe(gulp.dest(distPath))
 );
 
-gulp.task('copy-css-and-reload', gulp.series('copy-css', reload));
-gulp.task('copy-js-and-reload', gulp.series('copy-js', reload));
+gulp.task('copy-img', () =>
+    gulp.src([imgPath])
+        .pipe(changed(distPath))
+        .pipe(imagemin([
+                imageminPng(),
+                imageminJpg(),
+                imageminGif({
+                    interlaced: false,
+                    optimizationLevel: 3,
+                    colors:180
+                })
+        ]))
+        .pipe(gulp.dest(distPath))
+);
 
-gulp.task('build', gulp.series('copy-css', 'copy-js'));
+//gulp.task('copy-svg', function(){
+//    gulp.src(['assets/**/*.svg'])
+//        .pipe(svgmin())
+//        .pipe(gulp.dest(distPath))
+//});
+
+gulp.task('build', gulp.series('copy-css', 'copy-js', 'copy-img'));
 gulp.task('default', gulp.series('watch'));
-
-function reload(done) {
-    browserSync.reload();
-    done();
-}
